e220156f84aaab75f1cebec17f3ddda0bdaad70c
Wael Hajji <waelhajji.contact@gmail.com>
2023-05-17 14:13:11 +0100

    Week1Day3

--- python FTOS/Week1Day3/game.py
+++ python FTOS/Week1Day3/game.py
@@ -1,6 +1,35 @@
 from Lecture.barbarian import Barbarian
 from Lecture.knight import Knight
+import random
 
+# declare the instances
+barb = Barbarian("Conan")
+knight = Knight("Arthur")
 
-barb = Barbarian("Olaf")
-Knight = Knight("Arthur")
+while barb.health > 0 and knight.health > 0:
+    print("You are the mighty Barbarian, approach a Knight")
+    response = ""
+
+    while not response == "1" and not response == "2":
+        response = input("What do? \n 1)attack \n 2) REALLY ATTACK \n >>>>")
+        if response == "1":
+            barb.attack(knight)
+        elif response == "2":
+            barb.berserker_rage(knight)
+        else:
+            print(">>>> please choose a valid option")
+
+        # CPU respond
+        dice_roll = random.randint(1, 2)  # integer
+        if dice_roll == 1:
+            knight.attack(barb)
+        else:
+            knight.heal()
+
+# endgame
+if barb.health > 0:
+    print("Congratulations, you Won!")
+elif knight.health <= 0:
+    print("double KO")
+else:
+    print("the Knight Won")
